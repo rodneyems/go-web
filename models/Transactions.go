c540e5577b31a6c3d119f574849cc6c9617aa873
rodneyems <rodney.martins@live.com>
2022-05-31 12:44:15 -0300

    feature: add custom errors, update validation and use data in memory

--- models/Transactions.go
+++ models/Transactions.go
@@ -1,10 +1,13 @@
 package models
 
+var Transactions []Transaction
+var LastId int
+
 type Transaction struct {
 	Id       int     `json:"id"`
-	Currency string  `json:"currency"`
-	Price    float64 `json:"price"`
-	Issuer   string  `json:"issuer"`
-	Receiver string  `json:"receiver"`
-	Date     string  `json:"date"`
+	Currency string  `json:"currency" binding:"required"`
+	Price    float64 `json:"price" binding:"required"`
+	Issuer   string  `json:"issuer" binding:"required"`
+	Receiver string  `json:"receiver" binding:"required"`
+	Date     string  `json:"date" binding:"required"`
 }
